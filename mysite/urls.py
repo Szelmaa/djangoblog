f0e4fea7c1ec722695bfb009492afb89e41eeba8
pancia <pancia@example.com>
2024-01-18 09:33:08 +0100

    dodanie nowych plików oraz szablonów html

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for mysite project.
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+
 ]
+handler404 = 'blog.views.error_404_view'
+
+if settings.DEBUG:
+     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
